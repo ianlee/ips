3e697a28e1ca72ac3f08b67dc5702428bd4f25e9
luketao <coolman0893@gmail.com>
2014-03-08 14:33:06 -0800

    Sorting works in ids.py

--- firewall_ids.py
+++ firewall_ids.py
@@ -1,8 +1,14 @@
 import re
 
+def cutoffTime():
+	return 0
+
+
+
 with open("IP_list.txt", "r") as IP_list:
 	data = IP_list.readlines()
 
 	for line in data:
 		IP_and_time = re.split(' |\n', line)
-		print IP_and_time
+		
+
--- ids.py
+++ ids.py
@@ -1,29 +1,34 @@
 import time
 from datetime import datetime
 import re
-import os
+from operator import itemgetter
 
 def parseDate(line):
 	tokens = re.split('  | |\n', line)
-	date_format = ' '.join(tokens[0:3]) + " " + str(datetime.now().year)
-	the_date = str(int(time.mktime(time.strptime(date_format,'%b %d %H:%M:%S %Y'))))
-	return the_date
+	dateFormat = ' '.join(tokens[0:3]) + " " + str(datetime.now().year)
+	epochDate = str(int(time.mktime(time.strptime(dateFormat,'%b %d %H:%M:%S %Y'))))
+	return epochDate
 
 
 def parseIP(line):
 	tokens = re.split('  | |\n', line)
 	return tokens[10]
 
-IP_list = open("IP_list.txt", "w")
+def writeListToFile(IPlist):
+	with open('IP_list.txt', 'w') as fList:
+		for el in IPlist:
+			fList.write('{0}\n'.format(' '.join(el)))
+
+IPlist = []
 
 with open("/var/log/secure") as fsecure:
 	for line in fsecure:
 		if "sshd" and "Failed password for" in line:
-			date = parseDate(line)
+			epochDate = parseDate(line)
 			IP = parseIP(line)
-			print IP, date
-			IP_list.write(IP + " " + date + "\n")
+			print IP, epochDate
+			IPlist.append([IP,epochDate])
 
-IP_list.close()
-
-#os.system("sh scan_secure.sh")
+IPlist.sort(key=itemgetter(0,1))
+writeListToFile(IPlist)
+#print IPlist
