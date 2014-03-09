4bdc911b8ccfa9041a86ed7f82d8d4dcc59fe38c
luketao <coolman0893@gmail.com>
2014-03-08 21:52:23 -0800

    Implemented iptables conditions of when to block and unblock

--- firewall_ids.py
+++ firewall_ids.py
@@ -1,7 +1,7 @@
 import linecache
 from datetime import datetime
 import time
-import os
+import subprocess, os
 
 def timeExpired(epoch, cutoffTime):
 	return True if((int(datetime.now().strftime("%s")) - epoch > cutoffTime)) else False 
@@ -20,14 +20,24 @@ with open("IP_list.txt", "r") as IP_list:
 		if cIP[0] != nIP[0]:
 			count += 1
 			if count > 4:
-				# Will find a way to check if the IP rule exist
-				os.system('logger -p authpriv.notice "*** Blocking SSH attempt from: %s"' % cIP[0])
-				os.system('iptables -A INPUT -s %s -j DROP' % cIP[0])
+				notFound = subprocess.Popen("iptables-save | grep -q %s; echo $?" % cIP[0], shell=True, stdout=subprocess.PIPE).communicate()[0]
+				if int(notFound): # if IP rule is not already found in iptables, append it
+					os.system('logger -p authpriv.notice "*** Blocking SSH attempt from: %s"' % cIP[0])
+					os.system('iptables -A INPUT -s %s -j DROP' % cIP[0])
+					print "Host ", cIP[0], " added to iptables"
+				else:
+					print "Host ", cIP[0], " already exist in iptables"
 			else:
+				notFound = subprocess.Popen("iptables-save | grep -q %s; echo $?" % cIP[0], shell=True, stdout=subprocess.PIPE).communicate()[0]
+				if int(notFound):
+					print "Host ", cIP[0], " does not exist in iptables"
+				else:
+					os.system('iptables -D INPUT -s %s -j DROP' % cIP[0])
+					print "Host ", cIP[0], " dropped in iptables"
 				# Will find a way to check if the IP rule doesn't exist
 				# //potentially can check vs different cutoff time for removal of block time
 				#//check vs current line's time stamp
-				os.system('iptables -D INPUT -s %s -j DROP' % cIP[0])
+				
 
 			count = 0
 			currentIP = nextIP
