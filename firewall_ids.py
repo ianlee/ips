3bac6d7de0601ac07c4c7c8ba9742b115cf1f821
ian <ian.sean.lee@gmail.com>
2014-03-10 16:27:38 -0700

    files

--- firewall_ids.py
+++ firewall_ids.py
@@ -37,7 +37,7 @@ cutoffTime = 3600 # 60 minutes
 count = 0
 currentIP = linecache.getline("IP_list.txt", 1)
 
-with open("IP_list.txt", "r") as IP_list:
+with open(os.path.dirname(os.path.realpath(__file__))+"/IP_list.txt", "r") as IP_list:
 	nextIP = IP_list.readline()
 	
 	for nextIP in IP_list:
