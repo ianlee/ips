fab154e9aaa94b5db33d14ad2d6e7b5aee2bb972
ian <ian.sean.lee@gmail.com>
2014-03-10 16:03:39 -0700

    fixed crontab script

--- add_cron.sh
+++ add_cron.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-sed '/ids.py/d' /etc/crontab 
+sed -i '/ids.py/d' /etc/crontab 
 
-if [$# -gt 0 && $1 !=""]; then
-	exit
-fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 
